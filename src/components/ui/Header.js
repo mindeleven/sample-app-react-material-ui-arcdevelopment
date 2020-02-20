9fe57b875ee102a2513a9e7154933c11f413505b
Jürgen Kober <mindeleven@posteo.de>
2020-02-20 18:01:23 +0100

    Added some inline styling to the header component.

--- src/components/ui/Header.js
+++ src/components/ui/Header.js
@@ -3,6 +3,7 @@ import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import useScrollTrigger from '@material-ui/core/useScrollTrigger';
+import { makeStyles } from '@material-ui/core/styles';
 
 function ElevationScroll(props) {
   const { children } = props;
@@ -16,16 +17,26 @@ function ElevationScroll(props) {
   });
 }
 
+const useStyles = makeStyles(theme => ({
+  toolbarMargin: {
+    ...theme.mixins.toolbar
+  }
+}));
+
 export default function Header (props) {
+  const classes = useStyles();
   return (
-    <ElevationScroll>
-      <AppBar position="fixed">
-        <Toolbar>
-          <Typography variant="h3">
-            Arc Development
-          </Typography>
-        </Toolbar>
-      </AppBar>
-    </ElevationScroll>
+    <React.Fragment>
+      <ElevationScroll>
+        <AppBar position="fixed">
+          <Toolbar>
+            <Typography variant="h3">
+              Arc Development
+            </Typography>
+          </Toolbar>
+        </AppBar>
+      </ElevationScroll>
+      <div className={classes.toolbarMargin} />
+    </React.Fragment>
   );
 }
