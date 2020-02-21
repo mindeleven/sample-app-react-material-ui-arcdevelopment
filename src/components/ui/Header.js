498d5694878c27e52c5c1cc2b620d9dfd9cbdf09
Jürgen Kober <mindeleven@posteo.de>
2020-02-21 10:57:38 +0100

    Replaced typography element in header component with company logo.

--- src/components/ui/Header.js
+++ src/components/ui/Header.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
 import useScrollTrigger from '@material-ui/core/useScrollTrigger';
 import { makeStyles } from '@material-ui/core/styles';
+
+import logo from '../../assets/logo.svg';
 
 function ElevationScroll(props) {
   const { children } = props;
@@ -30,9 +31,7 @@ export default function Header (props) {
       <ElevationScroll>
         <AppBar position="fixed">
           <Toolbar>
-            <Typography variant="h3">
-              Arc Development
-            </Typography>
+            <img alt="company logo" src="{logo}" />
           </Toolbar>
         </AppBar>
       </ElevationScroll>
