a38b4b8b20140e8873ba1e0ada1669034322fbf1
Jürgen Kober <mindeleven@posteo.de>
2020-03-08 20:47:06 +0100

    Adding a useEffect hook to prevent inconsistencies in the navigation when refreshing the page.

--- src/components/ui/Header.js
+++ src/components/ui/Header.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import useScrollTrigger from '@material-ui/core/useScrollTrigger';
@@ -6,6 +6,7 @@ import { makeStyles } from '@material-ui/core/styles';
 import Tabs from '@material-ui/core/Tabs';
 import Tab from '@material-ui/core/Tab';
 import Button from '@material-ui/core/Button';
+import { Link } from 'react-router-dom';
 
 import logo from '../../assets/logo.svg';
 
@@ -54,6 +55,20 @@ export default function Header (props) {
     setValue(value);
   }
 
+  useEffect(() => {
+    if (window.location.pathname === '/' && value !== 0) {
+      setValue(0);
+    } else if (window.location.pathname === '/services' && value !== 1) {
+      setValue(1);
+    } else if (window.location.pathname === '/revolution' && value !== 2) {
+      setValue(2);
+    } else if (window.location.pathname === '/about' && value !== 3) {
+      setValue(3);
+    } else if (window.location.pathname === '/contact' && value !== 4) {
+      setValue(4);
+    }
+  }, [value])
+
   return (
     <React.Fragment>
       <ElevationScroll>
@@ -66,11 +81,11 @@ export default function Header (props) {
               className={classes.tabContainer}
               indicatorColor="primary"
             >
-              <Tab className={classes.tab} label="Home" />
-              <Tab className={classes.tab} label="Services" />
-              <Tab className={classes.tab} label="The Revolution" />
-              <Tab className={classes.tab} label="About Us" />
-              <Tab className={classes.tab} label="Contact Us" />
+              <Tab className={classes.tab} component={Link} to="/" label="Home" />
+              <Tab className={classes.tab} component={Link} to="/services" label="Services" />
+              <Tab className={classes.tab} component={Link} to="/revolution" label="The Revolution" />
+              <Tab className={classes.tab} component={Link} to="/about" label="About Us" />
+              <Tab className={classes.tab} component={Link} to="/contact" label="Contact Us" />
             </Tabs>
             <Button variant="contained" color="secondary" className={classes.button}>
               Free Estimate
