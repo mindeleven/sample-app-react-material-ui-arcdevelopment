e512809bba9093fb351bdd63c89b06d13a7dfc49
Jürgen Kober <mindeleven@posteo.de>
2020-03-30 11:26:17 +0200

    Refactoring the List component.

--- src/components/ui/Header.js
+++ src/components/ui/Header.js
@@ -250,81 +250,23 @@ export default function Header (props) {
         classes={{ paper: classes.drawer }}
       >
         <List disablePadding>
-          <ListItem
-            onClick={() => {setOpenDrawer(false); setValue(0)}}
-            divider
-            button
-            component={Link}
-            to="/"
-            selected={value === 0}
-          >
-            <ListItemText
-              className={value === 0 ? [classes.drawerItem, classes.drawerItemSelected].join(' ') : classes.drawerItem}
-              disableTypography
+          {routes.map((route, index) => (
+            <ListItem
+              onClick={() => {setOpenDrawer(false); setValue(route.activeIndex)}}
+              divider
+              button
+              component={Link}
+              to={route.link}
+              selected={value === route.activeIndex}
             >
-              Home
-            </ListItemText>
-          </ListItem>
-          <ListItem
-            onClick={() => {setOpenDrawer(false); setValue(1)}}
-            divider
-            button
-            component={Link}
-            to="/services"
-            selected={value === 1}
-          >
-            <ListItemText
-              className={value === 1 ? [classes.drawerItem, classes.drawerItemSelected].join(' ') : classes.drawerItem}
-              disableTypography
-            >
-              Services
-            </ListItemText>
-          </ListItem>
-          <ListItem
-            onClick={() => {setOpenDrawer(false); setValue(2)}}
-            divider
-            button
-            component={Link}
-            to="/revolution"
-            selected={value === 2}
-          >
-            <ListItemText
-              className={value === 2 ? [classes.drawerItem, classes.drawerItemSelected].join(' ') : classes.drawerItem}
-              disableTypography
-            >
-              The Revolution
-            </ListItemText>
-          </ListItem>
-          <ListItem
-            onClick={() => {setOpenDrawer(false); setValue(3)}}
-            divider
-            button
-            component={Link}
-            to="/about"
-            selected={value === 3}
-          >
-            <ListItemText
-              className={value === 3 ? [classes.drawerItem, classes.drawerItemSelected].join(' ') : classes.drawerItem}
-              disableTypography
-            >
-              About Us
-            </ListItemText>
-          </ListItem>
-          <ListItem
-            onClick={() => {setOpenDrawer(false); setValue(4)}}
-            divider
-            button
-            component={Link}
-            to="/contact"
-            selected={value === 4}
-          >
-            <ListItemText
-              className={value === 4 ? [classes.drawerItem, classes.drawerItemSelected].join(' ') : classes.drawerItem}
-              disableTypography
-            >
-              Contact Us
-            </ListItemText>
-          </ListItem>
+              <ListItemText
+                className={value === route.activeIndex ? [classes.drawerItem, classes.drawerItemSelected].join(' ') : classes.drawerItem}
+                disableTypography
+              >
+                {route.name}
+              </ListItemText>
+            </ListItem>
+          ))}
           <ListItem
             className={classes.drawerItemEstimate}
             onClick={() => {setOpenDrawer(false); setValue(5)}}
