95abb842da1c19d5b1c22f3a85ab3b786ae46348
Jürgen Kober <mindeleven@posteo.de>
2020-03-30 10:38:09 +0200

    Refactoring the useEffect function for the highlighting of the selected menu items.

--- src/components/ui/Header.js
+++ src/components/ui/Header.js
@@ -152,13 +152,37 @@ export default function Header (props) {
   }
 
   const menuOptions = [
-    { name: 'Services', link: '/services' },
-    { name: 'Custom Software Development', link: '/customsoftware' },
-    { name: 'Mobile App Development', link: '/mobileapps' },
-    { name: 'Website Development', link: '/websites' }
+    { name: 'Services', link: '/services', activeIndex: 1, setSelectedIndex: 0 },
+    { name: 'Custom Software Development', link: '/customsoftware', activeIndex: 1, setSelectedIndex: 1 },
+    { name: 'Mobile App Development', link: '/mobileapps', activeIndex: 1, setSelectedIndex: 2 },
+    { name: 'Website Development', link: '/websites', activeIndex: 1, setSelectedIndex: 3 }
+  ];
+
+  const routes = [
+    {name: 'Home', link: '/', activeIndex: 0},
+    {name: 'Services', link: '/services', activeIndex: 1},
+    {name: 'The Revolution', link: '/revolution', activeIndex: 2},
+    {name: 'About Us', link: '/about', activeIndex: 3},
+    {name: 'Contact Us', link: '/contact', activeIndex: 4}
   ];
 
   useEffect(() => {
+    [...menuOptions, ...routes].forEach((route) => {
+      switch(window.location.pathname) {
+        case `${route.link}`:
+          if (value !== route.activeIndex ) {
+            setValue(route.activeIndex);
+            if (route.selectedIndex && route.selectedIndex !== selectedIndex) {
+              setSelectedIndex(route.selectedIndex);
+            }
+          }
+        break;
+        default:
+        break;
+      }
+    });
+/***
+
     switch(window.location.pathname) {
       case "/":
         if (value !== 0) {
@@ -207,7 +231,8 @@ export default function Header (props) {
       default:
         break;
     }
-  }, [value])
+  ***/
+}, [value, menuOptions, selectedIndex, routes])
 
   const tabs = (
     <React.Fragment>
