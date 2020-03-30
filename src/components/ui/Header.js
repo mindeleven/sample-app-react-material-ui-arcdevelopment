c046ca2e3ac5380b308ac4f7409cbf217cbedbae
Jürgen Kober <mindeleven@posteo.de>
2020-03-30 10:39:30 +0200

    Removing some code that has been replaced.

--- src/components/ui/Header.js
+++ src/components/ui/Header.js
@@ -181,58 +181,7 @@ export default function Header (props) {
         break;
       }
     });
-/***
-
-    switch(window.location.pathname) {
-      case "/":
-        if (value !== 0) {
-          setValue(0);
-        }
-        break;
-      case "/services":
-        if (value !== 1) {
-          setValue(1);
-          setSelectedIndex(0);
-        }
-        break;
-      case "/customsoftware":
-        if (value !== 1) {
-          setValue(1);
-          setSelectedIndex(1);
-        }
-        break;
-      case "/mobileapps":
-        if (value !== 1) {
-          setValue(1);
-          setSelectedIndex(2);
-        }
-        break;
-      case "/websites":
-        if (value !== 1) {
-          setValue(1);
-          setSelectedIndex(3);
-        }
-        break;
-      case "/revolution":
-        if (value !== 2) {
-          setValue(2);
-        }
-        break;
-      case "/about":
-        if (value !== 3) {
-          setValue(3);
-        }
-        break;
-      case "/contact":
-        if (value !== 4) {
-          setValue(4);
-        }
-        break;
-      default:
-        break;
-    }
-  ***/
-}, [value, menuOptions, selectedIndex, routes])
+  }, [value, menuOptions, selectedIndex, routes])
 
   const tabs = (
     <React.Fragment>
