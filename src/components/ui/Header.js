07a68397647357e2cadcbbdf41cb89c1aa6efe67
Jürgen Kober <mindeleven@posteo.de>
2020-03-31 18:23:45 +0200

    Removing an unused import statement.

--- src/components/ui/Header.js
+++ src/components/ui/Header.js
@@ -16,7 +16,6 @@ import MenuIcon from '@material-ui/icons/Menu';
 import IconButton from '@material-ui/core/IconButton';
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
-import ListItemIcon from '@material-ui/core/ListItemIcon';
 import ListItemText from '@material-ui/core/ListItemText';
 
 import logo from '../../assets/logo.svg';
