00165fa5f0f28957ecb0388d9e74fce86ef1fe2f
Jürgen Kober <mindeleven@posteo.de>
2020-03-31 18:22:20 +0200

    Some more zIndex magic to make the drawer appear between header and footer.

--- src/components/ui/Footer.js
+++ src/components/ui/Footer.js
@@ -1,10 +1,24 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 
+import footerAdornment from '../../assets/Footer Adornment.svg';
+
 const useStyles = makeStyles(theme => ({
   footer: {
     backgroundColor: theme.palette.common.blue,
-    width: '100%'
+    width: '100%',
+    zIndex: 1302,
+    position: "relative"
+  },
+  adornment: {
+    width: "25em",
+    verticalAlign: "bottom",
+    [theme.breakpoints.down("md")]: {
+      width: "21em"
+    },
+    [theme.breakpoints.down("xs")]: {
+      width: "15em"
+    }
   }
 }));
 
@@ -13,7 +27,7 @@ export default function Footer () {
 
   return (
     <footer className={classes.footer}>
-      Example Footer
-    </footer> 
+      <img alt="black decorative slash" src={footerAdornment} className={classes.adornment} />
+    </footer>
   )
 }
