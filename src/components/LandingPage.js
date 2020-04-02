e46a8c21b5f8a8dfd8fed3010a5a12358dacb478
Jürgen Kober <mindeleven@posteo.de>
2020-04-02 14:26:58 +0200

    Elaborating the layout of the landing page.

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import Lottie from 'react-lottie';
 import { makeStyles } from '@material-ui/core/styles';
+import Grid from '@material-ui/core/Grid';
+import Button from '@material-ui/core/Button';
 
 import animationData from '../animations/landinganimation/data';
 
@@ -21,6 +23,25 @@ export default function LandingPage() {
   };
 
   return (
-    <Lottie options={defaultOptions} height="100%" width="100%" />
+    <Grid container direction="column">
+      <Grid item>
+        <Grid container direction="row">
+          <Grid item>
+            <div>Bringing West Coast Technology<br /> to the Midwest</div>
+            <Grid container>
+              <Grid item>
+                <Button variant="contained">Free Estmate</Button>
+              </Grid>
+              <Grid item>
+                <Button variant="outlined">Learn more</Button>
+              </Grid>
+            </Grid>
+          </Grid>
+          <Grid item>
+            <Lottie options={defaultOptions} height="100%" width="100%" />
+          </Grid>
+        </Grid>
+      </Grid>
+    </Grid>
   )
 }
