dd5bdf861f966c7822c4518f06cdd81888744d00
Jürgen Kober <mindeleven@posteo.de>
2020-04-19 15:29:22 +0200

    Some more fine tuning for the look & feel of the Revolution block (5th section of the landing page).

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -101,7 +101,15 @@ const useStyles = makeStyles(theme => ({
     position: "absolute",
     boxShadow: theme.shadows[10],
     borderRadius: 15,
-    padding: "10em"
+    padding: "10em",
+    [theme.breakpoints.down("sm")]: {
+      paddingTop: "8em",
+      paddingBottom: "8em",
+      paddingLeft: 0,
+      paddingRight: 0,
+      borderRadius: 0,
+      width: "100%"
+    }
   }
 }));
 
@@ -244,12 +252,12 @@ export default function LandingPage() {
         </Grid>
       </Grid>
       <Grid item> {/*----- Revolution Block -----*/}
-        <Grid container style={{height: "100em"}} alignItems="center" justify="center">
+        <Grid container style={{ height: "100em", marginTop: "12em" }} alignItems="center" justify="center">
           <Card className={classes.revolutionCard}>
             <CardContent>
               <Grid container direction="column" style={{textAlign: "center"}}>
                 <Grid item>
-                  <Typography variant="h3">
+                  <Typography variant="h3" gutterBottom>
                     The Revolution
                   </Typography>
                 </Grid>
